43ab8fc56a2f22d1976b44632e147a553c389221
shahzaibk23 <shahzaibceo@gmail.com>
2022-04-23 15:48:35 +0500

    peripheral script modified acc to new devices

--- peripheralScript.py
+++ peripheralScript.py
@@ -1,7 +1,87 @@
 #!/usr/bin/python3
 
-def peripheralSwitch():
-	first_comb = """
+# def peripheralSwitch():
+# 	first_comb = """
+# 	package caravan.bus.common
+# 	import chisel3._
+# 	import chisel3.experimental.ChiselEnum
+
+# 	object Peripherals extends ChiselEnum {
+# 	val DCCM = Value(0.U)
+# 	val GPIO = Value(1.U)
+# 	}"""
+
+# 	second_comb = """
+# 	package caravan.bus.common
+# 	import chisel3._
+# 	import chisel3.experimental.ChiselEnum
+
+# 	object Peripherals extends ChiselEnum {
+# 	val DCCM = Value(0.U)
+# 	val GPIO = Value(1.U)
+# 	val SPI  = Value(2.U)
+# 	}"""
+
+# 	third_comb = """
+# 	package caravan.bus.common
+# 	import chisel3._
+# 	import chisel3.experimental.ChiselEnum
+
+# 	object Peripherals extends ChiselEnum {
+# 	val DCCM = Value(0.U)
+# 	val GPIO = Value(1.U)
+# 	val UART = Value(2.U)
+# 	}"""
+
+# 	fourth_comb = """
+# 	package caravan.bus.common
+# 	import chisel3._
+# 	import chisel3.experimental.ChiselEnum
+
+# 	object Peripherals extends ChiselEnum {
+# 	val DCCM = Value(0.U)
+# 	val GPIO = Value(1.U)
+# 	val SPI  = Value(2.U)
+# 	val UART = Value(3.U)
+# 	}"""
+
+# 	import json,os
+# 	from xmlrpc.client import Boolean
+# 	file = open("src/main/scala/config.json", "r")
+# 	data = json.load(file)
+# 	print(data)
+# 	file.close()
+# 	uart = bool(data["uart"])
+# 	spi_flash = bool(data["spi_flash"])
+# 	print(spi_flash)
+# 	caravan_path = "caravan/src/main/scala/caravan/bus/common/PeripheralsMap.scala"
+# 	if not uart and not spi_flash:
+# 		print(first_comb)
+# 		caravan1 = open(caravan_path, "w")
+# 		caravan1.write(first_comb)
+# 		caravan1.close()
+
+# 	elif not uart and spi_flash:
+# 		print(second_comb)
+# 		caravan1 = open(caravan_path, "w")
+# 		caravan1.write(second_comb)
+# 		caravan1.close()
+
+# 	elif uart and not spi_flash:
+# 		print(third_comb)
+# 		caravan1 = open(caravan_path, "w")
+# 		caravan1.write(third_comb)
+# 		caravan1.close()
+
+# 	elif uart and spi_flash:
+# 		print(fourth_comb)
+# 		caravan1 = open(caravan_path, "w")
+# 		caravan1.write(fourth_comb)
+# 		caravan1.close()
+
+import json
+
+base = """
 	package caravan.bus.common
 	import chisel3._
 	import chisel3.experimental.ChiselEnum
@@ -11,72 +91,58 @@ def peripheralSwitch():
 	val GPIO = Value(1.U)
 	}"""
 
-	second_comb = """
-	package caravan.bus.common
-	import chisel3._
-	import chisel3.experimental.ChiselEnum
-
+dccm = """
 	object Peripherals extends ChiselEnum {
 	val DCCM = Value(0.U)
-	val GPIO = Value(1.U)
-	val SPI  = Value(2.U)
-	}"""
+"""
+closer = "}"
+file = open("src/main/scala/config.json")
+data = json.load(file)
+file.close()
 
-	third_comb = """
-	package caravan.bus.common
-	import chisel3._
-	import chisel3.experimental.ChiselEnum
+main_str = base + dccm
+dev_count = 1
 
-	object Peripherals extends ChiselEnum {
-	val DCCM = Value(0.U)
-	val GPIO = Value(1.U)
-	val UART = Value(2.U)
-	}"""
+if data["gpio"] == 1:
+	gpio = f"""
+		val GPIO = Value({dev_count}.U)
+	"""
+	main_str += gpio
+	dev_count += 1
 
-	fourth_comb = """
-	package caravan.bus.common
-	import chisel3._
-	import chisel3.experimental.ChiselEnum
+if data["spi_flash"] == 1:
+	spi_f = f"""
+		val SPI  = Value({dev_count}.U)
+	"""
+	main_str += spi_f
+	dev_count += 1
 
-	object Peripherals extends ChiselEnum {
-	val DCCM = Value(0.U)
-	val GPIO = Value(1.U)
-	val SPI  = Value(2.U)
-	val UART = Value(3.U)
-	}"""
+if data["uart"] == 1:
+	uart = f"""
+		val UART = Value({dev_count}.U)
+	"""
+	main_str += uart
+	dev_count += 1
 
-	import json,os
-	from xmlrpc.client import Boolean
-	file = open("src/main/scala/config.json", "r")
-	data = json.load(file)
-	print(data)
-	file.close()
-	uart = bool(data["uart"])
-	spi_flash = bool(data["spi_flash"])
-	print(spi_flash)
-	caravan_path = "caravan/src/main/scala/caravan/bus/common/PeripheralsMap.scala"
-	if not uart and not spi_flash:
-		print(first_comb)
-		caravan1 = open(caravan_path, "w")
-		caravan1.write(first_comb)
-		caravan1.close()
+if data["timer"] == 1:
+	timer = f"""
+		val TIMER = Value({dev_count}.U)
+	"""
+	main_str += timer
+	dev_count += 1
 
-	elif not uart and spi_flash:
-		print(second_comb)
-		caravan1 = open(caravan_path, "w")
-		caravan1.write(second_comb)
-		caravan1.close()
+if data["i2c"] == 1:
+	i2c = f"""
+		val I2C = Value({dev_count}.U)
+	"""
+	main_str += i2c
+	dev_count += 1
 
-	elif uart and not spi_flash:
-		print(third_comb)
-		caravan1 = open(caravan_path, "w")
-		caravan1.write(third_comb)
-		caravan1.close()
+main_str += closer
 
-	elif uart and spi_flash:
-		print(fourth_comb)
-		caravan1 = open(caravan_path, "w")
-		caravan1.write(fourth_comb)
-		caravan1.close()
+print(main_str)
 
-peripheralSwitch()
+caravan_path = "caravan/src/main/scala/caravan/bus/common/PeripheralsMap.scala"
+file = open(caravan_path, "w")
+file.write(main_str)
+file.close()
