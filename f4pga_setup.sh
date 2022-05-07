f26713a32818c7a4a667430a518962adc5139e64
shahzaibk23 <shahzaibceo@gmail.com>
2022-05-07 10:45:08 +0500

    bug in script corrected

--- f4pga_setup.sh
+++ f4pga_setup.sh
@@ -3,7 +3,7 @@ export INSTALL_DIR=~/opt/f4pga
 export FPGA_FAM=xc7
 bash conda_installer.sh -u -b -p $INSTALL_DIR/$FPGA_FAM/conda;
 source "$INSTALL_DIR/$FPGA_FAM/conda/etc/profile.d/conda.sh";
-conda env create -f $INSTALL_DIR/$FPGA_FAM/environment.yml
+conda env create -f $INSTALL_DIR/$FPGA_FAM/install/environment.yml
 mkdir -p $INSTALL_DIR/xc7/install
 wget -qO- https://storage.googleapis.com/symbiflow-arch-defs/artifacts/prod/foss-fpga-tools/symbiflow-arch-defs/continuous/install/20220406-185509/symbiflow-arch-defs-install-3ef4188.tar.xz | tar -xJC $INSTALL_DIR/xc7/install
 wget -qO- https://storage.googleapis.com/symbiflow-arch-defs/artifacts/prod/foss-fpga-tools/symbiflow-arch-defs/continuous/install/20220406-185509/symbiflow-arch-defs-xc7a50t_test-3ef4188.tar.xz | tar -xJC $INSTALL_DIR/xc7/install
